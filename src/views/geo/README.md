c849d755e93300233424e3f38266d41747a96e67
liyf7218 <liyf7218@163.com>
2018-12-16 16:16:31 +0800

    添加百度地图实例，详情查看 views/geo/README.md

--- src/views/geo/README.md
+++ src/views/geo/README.md
@@ -1,2 +1,16 @@
-## 在 Vue 中引入 Echarts ##
+## 地图配置 ##
+
+### 基本选择： Echarts ###
+
+官网 [ECharts](http://www.echartsjs.com/index.html)
+
+
 查看 [Vue系列——在vue项目中使用echarts](https://blog.csdn.net/mr_wuch/article/details/70225364)
+
+### 使用百度地图 BMap ###
+
+官网 [百度地图开发平台 JavaScript API](http://lbsyun.baidu.com/index.php?title=jspopular)
+
+获取开发密钥 [百度地图开发平台 密钥](http://lbsyun.baidu.com/apiconsole/key)
+
+实例 [百度地图开放平台 实例](http://lbsyun.baidu.com/jsdemo.htm#a1_2)
--- src/views/geo/index.vue
+++ src/views/geo/index.vue
@@ -1,10 +1,11 @@
 <template>
   <section class="main" v-loading="loading">
-  	
+    <div id="geoChart" class="geo-chart"></div>
   </section>
 </template>
 <script>
 export default {
+  name: 'geoChart',
   data() {
     return {
       // loading 模态层
@@ -13,14 +14,48 @@ export default {
     }
   },
   methods: {
-
+    drawMap() {
+      console.log('drawMap')
+      this.geoChart.setOption(this.geoChartOptions)
+    }
   },
-  created() {
+  mounted() {
+    if(BMap){
+      // 使用这个 BMap
+      console.log('BMap', BMap)
+    }else{
+      this.errorMessage('百度地图设置错误,请检查相关设置!')
+      return;
+    }
+
+    //百度地图api功能：
+
+    // 创建map实例
+    let map = new BMap.Map('geoChart');
+    // 初始化地图，设置中心点坐标和地图级别
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+    // 添加地图类型控件
+    map.addControl(new BMap.MapTypeControl({
+      mapTypes: [
+        BMAP_NORMAL_MAP,
+        BMAP_HYBRID_MAP
+      ]
+    }));
+
+    // 设置地图显示的城市，此项是必须设置的
+    map.setCurrentCity('北京');
+    // 开启鼠标滚轮缩放
+    map.enableScrollWheelZoom(true);
   }
 }
 
 </script>
 <style scoped>
-
+.geo-chart {
+  width: 100%;
+  min-width: 500px;
+  height: 100%;
+  min-height: 800px;
+}
 
 </style>
